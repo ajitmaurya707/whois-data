96cb42fe9a13107e43cb02b9de71f349479280e5
ajitmaurya707 <ajitmaurya707@gmail.com>
2018-07-04 15:59:18 +0530

    Add files via upload

---
+++ whoisinfodata.py
@@ -0,0 +1,11 @@
+import whois
+import webbrowser as wb
+from json2html import *
+
+w = whois.whois('https://intellymail.com')
+d = json2html.convert(json = w)
+
+f = open("index.html","w")
+w = f.write(d)
+f.close()
+wb.open("index.html")
